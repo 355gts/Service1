893723c7db81c3faa369bebf0c3ca9fabecfd69d
355gts <blackmore__s@hotmail.com>
2020-12-16 22:01:43 +0000

    Added a .net solution

---
+++ Service1/Service1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Service.Common;
+using System;
+
+namespace Service1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            var screenWriter = new ScreenWriter(new DoSomething());
+
+            screenWriter.WriteMessage(5).GetAwaiter().GetResult();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Service1/Service1/ScreenWriter.cs
@@ -0,0 +1,21 @@
+﻿using Service.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace Service1
+{
+    public class ScreenWriter
+    {
+        private readonly IDoSomething _doSomething;
+
+        public ScreenWriter(IDoSomething doSomething)
+        {
+            _doSomething = doSomething ?? throw new ArgumentNullException(nameof(doSomething));
+        }
+
+        public async Task WriteMessage(int times)
+        {
+            await _doSomething.PrintMessage(times);
+        }
+    }
+}
